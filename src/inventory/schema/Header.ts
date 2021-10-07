7a22ef75c7fa038a6cd2583dbef0f67a0e572467
Jackson Machado <jacpts@gmail.com>
2021-10-07 17:46:31 -0300

    fix: change header type and export converters

--- src/inventory/schema/Header.ts
+++ src/inventory/schema/Header.ts
@@ -1,28 +1,26 @@
 import * as t from 'io-ts'
 import { datetime } from '../../custom-types'
 
-export const Header = t.type({
-    Header: t.intersection([
-        t.type({
-            UUID: t.string,
-            Type: t.string,
-            Transaction: t.string,
-            StandardVersion: t.string,
-            SourceApplication: t.string,
-            ProductName: t.string,
-            ProductVersion: t.string,
-            CompanyId: t.string,
-            BranchId: t.string,
-            GeneratedOn: datetime,
-            DeliveryType: t.string,
-            Event: t.string
-        }),
-        t.partial({
-            SubType: t.string,
-            Version: t.string,
-            CompanySharingMode: t.string,
-            BusinessUnitySharingMode: t.string,
-            BranchSharingMode: t.string
-        })
-    ])
-})
+export const Header = t.intersection([
+    t.type({
+        UUID: t.string,
+        Type: t.string,
+        Transaction: t.string,
+        StandardVersion: t.string,
+        SourceApplication: t.string,
+        ProductName: t.string,
+        ProductVersion: t.string,
+        CompanyId: t.string,
+        BranchId: t.string,
+        GeneratedOn: datetime,
+        DeliveryType: t.string,
+        Event: t.string
+    }),
+    t.partial({
+        SubType: t.string,
+        Version: t.string,
+        CompanySharingMode: t.string,
+        BusinessUnitySharingMode: t.string,
+        BranchSharingMode: t.string
+    })
+])
