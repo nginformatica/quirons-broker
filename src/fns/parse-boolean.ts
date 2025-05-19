fef75efe51f0fb0cbad0665ad41e756bec1e1408
Daniela Marioti <daniela.oliveira@ngi.com.br>
2025-05-19 14:30:16 -0300

    fix: movement parse boolean

--- src/fns/parse-boolean.ts
+++ src/fns/parse-boolean.ts
@@ -1,6 +1,6 @@
-export function parseBoolean(value: unknown): boolean {
+export function parseBoolean(value: unknown): boolean | undefined {
     if (typeof value === 'string') {
         return value === 'true' || value === '1'
     }
-    return !!value
+    return value as boolean | undefined
 }
